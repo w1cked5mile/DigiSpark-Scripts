e9f4cb753d31dc3e285f8372ee7c13bc7565a665
W1cked5mile <w1cked5milejones@gmail.com>
2024-04-20 17:32:35 -0400

    Update repository with new files

--- FakeUpdate_Win10ue/FakeUpdate_Win10ue.ino
+++ FakeUpdate_Win10ue/FakeUpdate_Win10ue.ino
@@ -4,16 +4,10 @@ void setup() {
   //empty
 }
 void loop() {
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.print("http://fakeupdate.net/win11");
+  DigiKeyboard.print("http://fakeupdate.net/win10ue");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
--- FakeUpdate_Win11/FakeUpdate_Win11.ino
+++ FakeUpdate_Win11/FakeUpdate_Win11.ino
@@ -4,18 +4,12 @@ void setup() {
   //empty
 }
 void loop() {
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(3000);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.print("http://fakeupdate.net/win11");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
   for(;;){ /*empty*/ }
 }
